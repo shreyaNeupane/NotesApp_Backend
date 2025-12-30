f6e5c46bae2c32f55e6ce3080841659c098e6389
shreyaNeupane <shreyaneupane49@gmail.com>
2025-12-30 09:41:40 +0545

    authmiddleware

---
+++ middleware/authMiddlware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddlware = async (req, res, next) => {
+  try {
+    //getting authorization form frontend
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json("please provide token");
+    }
+    // extrct token = spliting bearer & token
+    const token = authHeader.split(" ")[1];
+    //verfying token  using secert key
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    //attach decoded data to request
+    req.user = decoded;
+//move to next middleware/controller
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("internal error");
+  }
+};
+module.exports = authMiddlware;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {registerController,loginController} = require("../Controller/userController");
+const {registerController,loginController} = require("../controller/userController");
 
 
 const router = express.Router();
